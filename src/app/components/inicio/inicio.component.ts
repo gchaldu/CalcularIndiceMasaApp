711f0be02500cd574da1d69afa802aa231af603c
Gabriel Chaldu <gabrielchaldu@gmail.com>
2023-05-17 14:49:42 -0300

    final del componente inicio

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+  public altura: number = 170;
+  public peso: number = 70;
+  public edad: number = 40;
+  public indice: number = 0;
 
+  public sumarEdad():number
+  {
+    if (this.edad >= 0){
+      this.edad+=1;
+    }
+    return this.edad;
+  }
+
+  public restarEdad():number
+  {
+    if (this.edad > 0){
+      this.edad-=1;
+    }
+    return this.edad;
+  }
+
+  public sumarPeso():number
+  {
+    if (this.peso >= 0){
+      this.peso+=1;
+    }
+    return this.peso}
+
+  public restarPeso():number
+  {
+    if (this.peso > 0){
+      this.peso-=1;
+    }
+    return this.peso;
+  }
+
+  public alturaPersona(event:any):void
+  {
+    this.altura = event.target.value;
+  }
+
+  public calcularIndiceMasaCorporal():number
+  {
+    return this.indice = this.peso / Math.pow(this.altura, 2);
+  }
 }
